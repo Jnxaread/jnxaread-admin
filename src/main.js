66a82f52c230bd3d53ed718f46bce39b0316a531
LiSong-ux <song.li@mycool.tv>
2022-03-21 16:57:38 +0800

    优化代码结构

--- src/main.js
+++ src/main.js
@@ -8,13 +8,13 @@ import qs from 'qs';
 import moment from 'moment';
 import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
-import global from './global/global.js'
 import API from './global/api'
+import method from "@/global/method";
 
 Vue.use(VueAxios, axios);
 Vue.prototype.qs = qs;
 Vue.use(ViewUI);
-Vue.use(global);
+Vue.use(method);
 
 Vue.prototype.api = API;
 Vue.config.productionTip = false;
